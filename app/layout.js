1bbdaa725b11c7701ec4b61a68b8b3b45dfbeed2
Hadis <hadisematloob@yahoo.com>
2024-11-16 16:14:37 +0330

    load fonts

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,12 @@
 import Logo from "@/app/_components/Logo";
 import Navigation from "@/app/_components/Navigation";
 import "@/app/_styles/globals.css";
+import { Josefin_Sans } from "next/font/google";
+
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export const metadata = {
   title: {
@@ -12,7 +18,9 @@ export const metadata = {
 function RootLayout({ children }) {
   return (
     <html>
-      <body className="bg-primary-950 text-primary-100 min-h-screen">
+      <body
+        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen`}
+      >
         <header>
           <Logo />
         </header>
