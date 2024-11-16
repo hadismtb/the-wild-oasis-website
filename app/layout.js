f86fbd1354d635561f50c4b4153bd85d66238a5e
Hadis <hadisematloob@yahoo.com>
2024-11-16 16:30:01 +0330

    improve layout design

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
-import Logo from "@/app/_components/Logo";
 import Navigation from "@/app/_components/Navigation";
 import "@/app/_styles/globals.css";
 import { Josefin_Sans } from "next/font/google";
+import Header from "@/app/_components/Header";
 
 const josefin = Josefin_Sans({
   subsets: ["latin"],
@@ -19,13 +19,12 @@ function RootLayout({ children }) {
   return (
     <html>
       <body
-        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen`}
+        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen flex flex-col`}
       >
-        <header>
-          <Logo />
-        </header>
-        <Navigation />
-        <main>{children}</main>
+        <Header />
+        <div className="flex-1 px-8 py-12">
+          <main className="max-w-7xl mx-auto">{children}</main>
+        </div>
       </body>
     </html>
   );
